89a9f0091b0e520a65377ffe84d2fec75e6f672b
hasansjc <hasansjc@gmail.com>
2024-02-09 10:16:34 +0530

    checking conflict in ticekt3

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-console.log("First line from main");
+console.log("First line from main is changed in ticket3");
 console.log("Now Added from dev branch");
 console.log("Now Added2 from dev branch");
