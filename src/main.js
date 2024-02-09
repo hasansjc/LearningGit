09b4007eab564a2fd5bc728fce34e1f0eb4e90fa
hasansjc <hasansjc@gmail.com>
2024-02-09 09:58:35 +0530

    ticket 2pushed again

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ console.log("First line from main");
 console.log("Now Added from dev branch");
 console.log("Now Added2 from dev branch");
 console.log("added ticket 2 changes");
+console.log("Here again we should see the conflict");
