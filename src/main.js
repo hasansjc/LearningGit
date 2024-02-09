4f106cf443285cc32fb661340d39b3e257808c49
hasansjc <hasansjc@gmail.com>
2024-02-09 11:10:17 +0530

    resolved conflict

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-console.log("First line from main changed");
+console.log("First line from main");
 console.log("Now Added from dev branch");
 console.log("Now Added2 from dev branch");
 console.log("pushed from ticekt 4");
